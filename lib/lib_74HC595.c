11f63de894d32cd8f66258c3da60118cfff62fbd
mur4ik18 <o.o.kotov1999@gmail.com>
2023-07-18 11:45:27 +0200

    Refactor code
    in lib_74HC595.c/.h

--- lib/lib_74HC595.c
+++ lib/lib_74HC595.c
@@ -1,29 +1,15 @@
 #include "lib_74HC595.h"
 #include "Mega2560.h"
+#include "print.h"
 
 #include <stdint.h>
-
-
-void prepareSending(int8_t latchPin, int8_t resetPin, int8_t outputEnablePin) {
-    digitalWrite(latchPin, OFF);
-    digitalWrite(resetPin, ON);
-    digitalWrite(outputEnablePin, OFF);
-}
+#include <stdio.h>
 
 void shiftOut(int8_t dataPin, int8_t clockPin, uint8_t data) {
-    uint8_t bittsArray[7] = {0,0,0,0,0,0,0};
-    bitsToArray(data, bittsArray);
-
-    for(int8_t i = 0; i <= 8; i++) {
-        true ? digitalWrite(dataPin, ON): digitalWrite(dataPin, OFF);
+    for(int8_t i = 0; i < 8; i++) {
+        digitalWrite(dataPin, data & 1);
+        data <<= 1;
         digitalWrite(clockPin, ON);
         digitalWrite(clockPin, OFF);
     }
-
-}
-
-void endSending(int8_t latchPin, int8_t resetPin, int8_t outputEnablePin) {
-    // start sending
-    digitalWrite(latchPin, ON);
-    digitalWrite(outputEnablePin, ON);
 }
--- lib/lib_74HC595.h
+++ lib/lib_74HC595.h
@@ -3,8 +3,6 @@
 
 #include <stdint.h>
 
-void prepareSending(int8_t latchPin, int8_t resetPin, int8_t outputEnablePin);
 void shiftOut(int8_t dataPin, int8_t clockPin, uint8_t data);
-void endSending(int8_t latchPin, int8_t resetPin, int8_t outputEnablePin);
 
 #endif
