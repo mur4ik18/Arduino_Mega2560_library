60f49e56a0c5100b8eab1542b420eaf9a64b5945
mur4ik18 <o.o.kotov1999@gmail.com>
2023-07-21 14:18:13 +0200

    Delete old functions and write news
    In Mega2560.c delete:
        - digitalOutputMode()
        - digitalWrite()
    In Mega2560.h change:
        - tunrOn/turnOff
    end add new functions

--- lib/Mega2560.h
+++ lib/Mega2560.h
@@ -1,13 +1,24 @@
 /* Mega2560.h */
 #include <stdbool.h>
 #include <stdint.h>
+#include <avr/pgmspace.h>
+#include "pins_mega2560.h"
 
 #define ON true
 #define OFF false
+#define INPUT 2
+#define OUTPUT 3
 
-#define turnOn(port, value) (port |= port | (ON << value))
-#define turnOff(port, value) (port |= port & ~(ON << value))
+// Here you should use number of digital port.
+#define digital_to_port(x) (pgm_read_byte( digital_ports + x ))
+#define getBit(x) (pgm_read_byte( digital_pin_mask + x ))
+// for getting Register and Pin you should use digital_to_port. 
+#define getReg(x) (pgm_read_byte( ddrx + x ))
+#define getPin(x) (pgm_read_byte( pinx + x ))
+// For getting Port you can use digital_to_port.
+#define getPort(x) (pgm_read_byte( portx + x ))
 
-void digitalOutputMode(int8_t port);
-void digitalWrite(int8_t port, bool value);
+#define turnOn(port) ( getPort(digital_to_port(port)) |= getPort(digital_to_port(port)) | (ON << getBit(port) ))
+#define turnOff(port) ( getPort(digital_to_port(port)) |= getPort(digital_to_port(port)) & ~(ON << getBit(port) ))
+
 void bitsToArray(uint8_t num, uint8_t *bitters);
