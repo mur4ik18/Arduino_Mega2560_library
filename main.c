113bc5a62b108f0019b082956a8adeb4751be427
mur4ik18 <o.o.kotov1999@gmail.com>
2023-07-21 14:16:57 +0200

    Merged confilct

--- main.c
+++ main.c
@@ -1,13 +1,6 @@
 #include "./lib/UART_driver.h"
 #include "./lib/Mega2560.h"
 #include <util/delay.h>
-<<<<<<< Updated upstream
-
-int main(void) {
-    USART_Init(MYUBRR);
-
-=======
-#include "./lib/lib_74HC595.h"
 
 #define DEBUG 1
 
@@ -27,39 +20,21 @@ const int8_t clockPin = 4;
 // const int resetPin = 6;
 
 void setup() {
-    digitalOutputMode(dataPin);
-    digitalOutputMode(latchPin);
-    digitalOutputMode(clockPin);
+//    digitalOutputMode(dataPin);
+//    digitalOutputMode(latchPin);
+//    digitalOutputMode(clockPin);
 
     USART_Init(MYUBRR);
-}
+};
 
 int main(void) {
     setup();
-    print("Hello world\n");
-
     
->>>>>>> Stashed changes
     while (1) {
         // digitalWrite(outputEnablePin, OFF);
         // digitalWrite(resetPin, ON);
 
         _delay_ms(1000);
-<<<<<<< Updated upstream
-        print("Hello world\n");
-=======
-        digitalWrite(latchPin, OFF);
-
-        shiftOut(dataPin, clockPin, 0b10110101);
-
-        digitalWrite(latchPin, ON);
-        _delay_ms(1000);
-
-        print("sended\n");
-        // digitalWrite(outputEnablePin, ON);
-        // digitalWrite(resetPin, OFF);
->>>>>>> Stashed changes
     }
     return 1;
 }
-
