ed73d973dea10ba5a5186ffe749be12227dc247e
mur4ik18 <o.o.kotov1999@gmail.com>
2023-07-27 13:54:46 +0200

    Add print in blink.c

--- examples/blink.c
+++ examples/blink.c
@@ -5,15 +5,20 @@
 #define LED 13
 
 void setup() {
-   pinMode(13, OUTPUT);
+    pinMode(13, OUTPUT);
+    USART_Init(MYUBRR);
+    USART_Transmit("Started");
 }
 
 int main(void) {
     setup();
     while(1) {
         turn(LED, ON);
+        print("LED On\n");
         _delay_ms(1000);
         turn(LED, OFF);
+        print("LED OFF\n");
+        _delay_ms(1000);
     }
     return 1;
 }
