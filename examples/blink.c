c7ae2dca1c9a385cb332480139bac5fe1ccda14a
remimimimimi <remimimimimi@protonmail.com>
2023-07-27 14:21:07 +0300

    Fix warnings

--- examples/blink.c
+++ examples/blink.c
@@ -2,13 +2,13 @@
 #include <util/delay.h>
 
 // digital 13 - used for working with blink
-const LED = 13;
+#define LED 13
 
 void setup() {
    pinMode(13, OUTPUT);
 }
 
-uint8_t main(void) {
+int main(void) {
     setup();
     while(1) {
         turn(LED, ON);
