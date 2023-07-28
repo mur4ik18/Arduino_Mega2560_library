8046130845b87cec4c4e486cea7d7e65133882dd
mur4ik18 <o.o.kotov1999@gmail.com>
2023-07-28 12:17:31 +0200

    Fix problems

--- lib/lib_74HC595.c
+++ lib/lib_74HC595.c
@@ -1,15 +1,18 @@
 #include "lib_74HC595.h"
 #include "Mega2560.h"
-#include "print.h"
 
 #include <stdint.h>
 #include <stdio.h>
 
 void shiftOut(int8_t dataPin, int8_t clockPin, uint8_t data) {
     for(int8_t i = 0; i < 8; i++) {
-        digitalWrite(dataPin, data & 1);
-        data <<= 1;
-        digitalWrite(clockPin, ON);
-        digitalWrite(clockPin, OFF);
+        if ((data & 128) != 0 ) {
+            turn(dataPin, ON);
+        } else {
+            turn(dataPin, OFF);
+        }
+        data = data << 1;
+        turn(clockPin, ON);
+        turn(clockPin, OFF);
     }
 }
